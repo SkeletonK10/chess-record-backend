b3cd9964985fac177cfa1a59321c6cf23cab8d7f
Deokgeun <poohbilly2@naver.com>
2022-05-19 18:26:24 +0900

    Fix project structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import Router from 'express';
 import { test } from '../lib/api/root';
+import game from './game';
 
 const router = Router();
 
 router.get("/", test);
+
+router.use("/game/", game);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
-import router from "./router/root";
+import router from "./router";
 
 const app = express();
 
 app.use("/", router);
 
-app.listen("8000", () => {
-    console.log("Server listening on port 8000");
-});
+export default app;
