5acfc7e5b7781ad01fc46386a365b785e9080cf4
skeletonk10 <poohbilly2@naver.com>
2022-07-06 02:05:13 +0900

    [API] Add GET /game/:id/

--- src/router/game/api.ts
+++ src/router/game/api.ts
@@ -35,6 +35,61 @@ export const getGameList = async (req: Request, res: Response, next: NextFunctio
     return next();
 };
 
+export const getGameView = async (req: Request, res: Response, next: NextFunction) => {
+    
+    // TODO: get game from DB /////////////////
+    // const limit: number = 20;
+    // const query = `
+    // SELECT id, white, black, result
+    //     FROM game
+    //     LIMIT ${limit}
+    // `;
+    // const client = await getConnection();
+    // const result = await client.query(query);
+    // const rows = result.rows;
+    //////////////////////////////////////////
+    
+    // TEST DATA //
+    
+    const rows = [
+        {
+            id: 3,
+            createdAt: '2022-07-05',
+            white: {
+                id: 1,
+                name: 'Gamer1',
+                rating: 1000,
+            },
+            black: {
+                id: 2,
+                name: 'Gamer2',
+                rating: 1050,
+            },
+            result: '1-0',
+            notation: '1. e4 e5 2. Nf3 Nf6 1-0',
+        },
+        {
+            id: 6,
+            createdAt: '2022-07-06',
+            white: {
+                id: 3,
+                name: 'Gamer3',
+                rating: 1000,
+            },
+            black: {
+                id: 4,
+                name: 'Gamer4',
+                rating: 1050,
+            },
+            result: '1-0',
+            notation: '1. e4 e5 2. Nf3 Nf6 1-0',
+        }
+    ]
+    const row = rows.find(x => x.id === Number(req.params.id));
+    res.json(row);
+    return next();
+};
+
 export const insertGame = (req: Request, res: Response, next: NextFunction) => {
     // TODO: insert game to DB ////////////////
     
--- src/router/game/index.ts
+++ src/router/game/index.ts
@@ -1,9 +1,11 @@
 import Router from 'express';
-import { test, getGameList, insertGame } from './api';
+import { test, getGameList, getGameView, insertGame } from './api';
 
 const router = Router();
 
 router.get("/", getGameList);
 router.post("/", insertGame);
+
+router.get("/:id/", getGameView);
 
 export default router;
