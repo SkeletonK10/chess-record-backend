61cd0daaa3421494c2000fa47f50013eef964752
skeletonk10 <poohbilly2@naver.com>
2022-07-07 18:45:00 +0900

    [API] Implement GET /game/:id/

--- src/router/game/api.ts
+++ src/router/game/api.ts
@@ -1,61 +1,65 @@
 import { Request, Response, NextFunction } from "express";
 
+import { getConnection } from "../../db";
+
 export const test = (req: Request, res: Response, next: NextFunction) => {
     res.send("Hello, world!");
 };
 
 export const getGameView = async (req: Request, res: Response, next: NextFunction) => {
+    const gameQuery = `
+    SELECT  id,
+            TO_CHAR(createdAt, 'YYYY-MM-DD') as createdAt,
+            TO_CHAR(playedAt, 'YYYY-MM-DD') as playedAt,
+            white,
+            black,
+            result,
+            notation,
+            description
+    FROM game
+    WHERE id=${req.params.id}
+    `;
     
-    // TODO: get game from DB /////////////////
-    // const limit: number = 20;
-    // const query = `
-    // SELECT id, white, black, result
-    //     FROM game
-    //     LIMIT ${limit}
-    // `;
-    // const client = await getConnection();
-    // const result = await client.query(query);
-    // const rows = result.rows;
+    const playerQuery = (pid: number) => `
+    SELECT  id,
+            name,
+            rating
+    FROM player
+    WHERE id=${pid}
+    `;
+    
+    const client = await getConnection();
+    const gameRows = await client.query(gameQuery);
+    let game = gameRows.rows[0];
+    const whiteRows = await client.query(playerQuery(game.white));
+    const blackRows = await client.query(playerQuery(game.black));
+    game.white = whiteRows.rows[0];
+    game.black = blackRows.rows[0];
     //////////////////////////////////////////
     
     // TEST DATA //
     
-    const rows = [
-        {
-            id: 3,
-            createdAt: '2022-07-05',
-            white: {
-                id: 1,
-                name: 'Gamer1',
-                rating: 1000,
-            },
-            black: {
-                id: 2,
-                name: 'Gamer2',
-                rating: 1050,
-            },
-            result: '1-0',
-            notation: '1. e4 e5 2. Nf3 Nf6 1-0',
-        },
-        {
-            id: 6,
-            createdAt: '2022-07-06',
-            white: {
-                id: 3,
-                name: 'Gamer3',
-                rating: 1000,
-            },
-            black: {
-                id: 4,
-                name: 'Gamer4',
-                rating: 1050,
-            },
-            result: '1-0',
-            notation: '1. e4 e5 2. Nf3 Nf6 1-0',
-        }
-    ]
-    const row = rows.find(x => x.id === Number(req.params.id));
-    res.json(row);
+    // const game = 
+    // {
+    //     id: 3,
+    //     createdAt: '2022-07-05',
+    //     white: {
+    //         id: 1,
+    //         name: 'Gamer1',
+    //         rating: 1000,
+    //     },
+    //     black: {
+    //         id: 2,
+    //         name: 'Gamer2',
+    //         rating: 1050,
+    //     },
+    //     result: '1-0',
+    //     notation: '1. e4 e5 2. Nf3 Nf6 1-0',
+    // };
+
+    // const row = rows.find(x => x.id === Number(req.params.id));
+    ///////////////////////////////////////////
+    res.json(game);
     return next();
 };
 
