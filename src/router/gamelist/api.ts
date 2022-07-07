e1bf706ad8db39415d3eef9896ee206caa9427b3
skeletonk10 <poohbilly2@naver.com>
2022-07-07 19:34:31 +0900

    [API] Update getGameList Query

--- src/router/gamelist/api.ts
+++ src/router/gamelist/api.ts
@@ -16,29 +16,31 @@ export const getGameList = async (req: Request, res: Response, next: NextFunctio
     const page: number = valid ? Number(req.params.page) : 1;
     const offset: number = (page - 1) * limit;
     const query = `
-    SELECT id, TO_CHAR(createdAt, 'YYYY-MM-DD') as createdAt, white, black, result, notation
-        FROM game
-        LIMIT ${limit}
-        OFFSET ${offset}
+    SELECT  G.id as id,
+            TO_CHAR(G.createdAt, 'YYYY-MM-DD') as createdAt,
+            W.name as white,
+            B.name as black,
+            G.result as result
+    FROM    game as G
+            JOIN
+            player as W ON G.white = W.id
+            JOIN
+            player as B ON G.black = B.id
+    ORDER BY id DESC
+    LIMIT ${limit}
+    OFFSET ${offset}
     `;
     const client = await getConnection();
-    const result = await client.query(query);
-    const rows = result.rows;
-    
-    //////////////////////////////////////////
-    
-    // TEST DATA //
-    // const rows =
-    // [
-    //     {
-    //         id: 3,
-    //         createdAt: new Date(Date.now()),
-    //         white: 'Gamer1',
-    //         black: 'Gamer2',
-    //         result: '1-0'
-    //     },
-    // ]
-    
-    res.json(rows);
-    return next();
+    try {
+        const result = await client.query(query);
+        client.end();
+        const rows = result.rows;
+        res.json(rows);
+        return next();
+    } catch (err) {
+        console.log("Error occured while fetching data");
+        client.end();
+        res.json();
+        return next();
+    }
 };
