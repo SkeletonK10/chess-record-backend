6a9c8ee047029d1084dac6dcc5a935b5304706c5
skeletonk10 <poohbilly2@naver.com>
2022-07-30 03:26:31 +0900

    [API] Add POST /player/

--- src/router/player/api.ts
+++ src/router/player/api.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 
 import { getConnection } from "../../db";
+import { IUserInfo } from "../../lib/types";
+import { validateIUserInfo } from "../../lib/validate";
 
 export const test = (req: Request, res: Response, next: NextFunction) => {
     res.send("Hello, world!");
@@ -28,6 +30,54 @@ export const getPlayerView = async (req: Request, res: Response, next: NextFunct
                 msg: `PlayerView: Error occured while searching.\n${err}`,
             });
             console.log(`PlayerView: Error occured while searching.\n${err}`);
+        } finally {
+            client.end();
+            return next();
+        }
+    } catch (err) {
+        res.json({
+            code: 1001,
+            msg: "Error occured while DB connecting.",
+        });
+        console.log("Error occured while DB connecting.");
+        return next();
+    }
+}
+
+export const insertPlayer = async (req: Request, res: Response, next: NextFunction) => {
+    const body: IUserInfo = req.body;
+    const query = `
+    INSERT INTO player (name, userid)
+    VALUES ($1, $2)
+    `;
+    const validationRes = await validateIUserInfo(body);
+    if (validationRes.code !== 0) {
+        console.log(`Code ${validationRes.code}: Invalid query has rejected`)
+        res.json(validationRes);
+        return next();
+    }
+    const values = [
+        body.name,
+        body.userid];
+    
+    try {
+        const client = await getConnection();
+        try {
+            await client.query("BEGIN");
+            const ret = await client.query(query, values);
+            await client.query("COMMIT");
+            res.json({
+                code: 0,
+                msg: "Insert query executed successfully!",
+            });
+            console.log("Insert query executed successfully!");
+        } catch (err) {
+            await client.query("ROLLBACK");
+            res.json({
+                code: 1120,
+                msg: `InsertPlayer: Error occured while inserting.\n${err}`,
+            });
+            console.log(`InsertPlayer: Error occured while inserting.\n${err}`);
         } finally {
             client.end();
             return next();
--- src/router/player/index.ts
+++ src/router/player/index.ts
@@ -1,8 +1,9 @@
 import Router from 'express';
-import { test, getPlayerView } from './api';
+import { test, getPlayerView, insertPlayer } from './api';
 
 const router = Router();
 
+router.post("/", insertPlayer);
 router.get("/:id/", getPlayerView);
 
 export default router;
