dfd9e7127a514e6ef32268f8afd0db1947e06800
skeletonk10 <poohbilly2@naver.com>
2022-07-30 23:50:32 +0900

    [Env] Add dotenv

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,13 @@
 import { Client } from 'pg';
 
-import config from './dbconfig';
-
 export const getConnection = async () => {
-  const client = new Client(config);
+  const client = new Client({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBUSER,
+    port: Number(process.env.DBPORT),
+  });
   await client.connect();
   return client;
 };
