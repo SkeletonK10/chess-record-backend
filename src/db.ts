3ec22698baf4132b0f4199ebb0076f913b067ea1
skeletonk10 <poohbilly2@naver.com>
2022-08-28 03:51:37 +0900

    [DB] Fix DB idle timeout

--- src/db.ts
+++ src/db.ts
@@ -5,10 +5,11 @@ config();
 
 const pool = new Pool({
   host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: process.env.DBUSER,
-    port: Number(process.env.DBPORT),
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBUSER,
+  port: Number(process.env.DBPORT),
+  idleTimeoutMillis: 60000,
 })
 
 export const getConnection = async () => {
